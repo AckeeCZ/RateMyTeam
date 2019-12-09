6345299d5d086085bfe65ac365bbf09829ec0e3a
Marek Fořt <marekfort@me.com>
2019-12-09 17:37:07 +0100

    Add section.

--- RateMyTeam/ContractsView.swift
+++ RateMyTeam/ContractsView.swift
@@ -8,23 +8,28 @@
 
 import Foundation
 import SwiftUI
+import class UIKit.UITableView
 
 struct ContractsView: View {
     @ObservedObject var viewModel: AnyViewModel<ContractsState, ContractsInput>
+    
+    init(viewModel: AnyViewModel<ContractsState, ContractsInput>) {
+        UITableView.appearance().separatorStyle = .none
+        self.viewModel = viewModel
+    }
     
     var body: some View {
         NavigationView {
             List {
                 Section(header: Text("CURRENT")) {
                     ForEach(viewModel.state.contracts) { contract in
-                        HStack {
-                            ContractRow(contract: contract.contract).scaledToFill()
-                            NavigationLink(destination: RateView(viewModel: contract.viewModel)) {
-                                EmptyView()
-                            }
-                            Image(Asset.arrowRight.name)
-                        }
-                    }.listRowBackground(Color(Color.theme.background.color))
+                        ContractRow(contract: contract)
+                    }
+                }
+                Section(header: Text("PAST")) {
+                    ForEach(viewModel.state.contracts) { contract in
+                        ContractRow(contract: contract)
+                    }
                 }
             }
             .background(Color(Color.theme.background.color))
@@ -35,11 +40,27 @@ struct ContractsView: View {
 }
 
 struct ContractRow: View {
-    let contract: RateContract
+    let contract: ContractsState.ContractData
     
     var body: some View {
-        HStack {
-            Text(contract.id).theme.font(.bodyLarge)
+        ZStack {
+            HStack {
+                HStack {
+                    Text(contract.contract.id)
+                        .theme.font(.bodyLarge)
+                        .scaledToFill()
+                        .padding([.leading, .top, .bottom], 20)
+                }
+                NavigationLink(destination: RateView(viewModel: contract.viewModel)) {
+                    EmptyView()
+                }
+                Image(Asset.arrowRight.name)
+                    .padding(.trailing, 20)
+            }
+            .background(Color.white)
+            .cornerRadius(6)
+            .shadow(color: Color.black.opacity(0.1), radius: 6, x: 0, y: 0)
         }
+        .listRowBackground(Color(Color.theme.background.color))
     }
 }
