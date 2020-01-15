151150cdfe1678cfd6ef3830df11ab1d8581f41c
Marek Fořt <marek.fort@ackee.cz>
2020-01-15 08:30:39 +0100

    Update contract code.

--- contract.py
+++ contract.py
@@ -11,7 +11,7 @@ class Ballot(sp.Contract):
         self.init(name = name, master = creator, ballot = initialBallot, voters = initialVoters, hasEnded = False, totalNumberOfVotes = 0, votesPerVoter = numberOfVotes)
         
 
-    @sp.entry_point
+    @sp.entryPoint
     def vote(self, params):
         sp.verify(self.data.hasEnded == False)
         sp.verify(self.data.voters.contains(sp.sender))
@@ -22,19 +22,19 @@ class Ballot(sp.Contract):
             self.data.totalNumberOfVotes += vote.value
         
         
-    @sp.entry_point
+    @sp.entryPoint
     def end(self, params):
         sp.verify(sp.sender == self.data.master)
         sp.verify(self.data.hasEnded == False)
         sp.for candidate in self.data.ballot.keys():
             sp.if self.data.ballot[candidate].numberOfVotes != 0:
-                sp.send(candidate, sp.splitTokens(sp.balance, sp.as_nat(self.data.ballot[candidate].numberOfVotes), sp.as_nat(self.data.totalNumberOfVotes)))
+                sp.send(candidate, sp.splitTokens(sp.balance, sp.asNat(self.data.ballot[candidate].numberOfVotes), sp.asNat(self.data.totalNumberOfVotes)))
         self.data.hasEnded = True
 
         
-@sp.add_test(name = "CandidatesTest")
+@addTest(name = "CandidatesTest")
 def test():
-    scenario = sp.test_scenario()
+    scenario = sp.testScenario()
     scenario.h1("Candidates")
     
     addr1 = sp.address("tz1VsSC7ZUxpTt9t18Cuae9NeL2xWTgX7Mix")
@@ -44,19 +44,6 @@ def test():
     addr5 = sp.address("tz1c2mnLJSwUYHNytq1XZ9YQT81ojca7YDYY")
     addr6 = sp.address("tz1c2mnLJSwUYHNytq1XZ9YQT81ojca7YDYY")
     
-    contract = Ballot("Design", addr5, [(addr1, "Winner"), (addr2, "Jirka"), (addr3, "Pepus")], [addr4, addr6], 100)
-    #sp.send(contract, sp.tez(1))
+    contract = Ballot("Design", addr5, [(addr1, "George"), (addr2, "Tim"), (addr3, "Gina")], [addr4, addr6], 100)
 
     scenario += contract
-    
-    #scenario += contract.vote(votes = {addr1: 1}).run(valid=False,sender = addr4)
-    
-    #scenario += contract.end().run(valid=False, sender = addr4)
-    #scenario += contract.end().run(sender = addr5)
-    #scenario += contract.end().run(sender = addr5)
-    
-    
-    
-    
-    
-    
